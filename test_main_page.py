bec5b696bf8cb5093298567a4c513a242cdb676c
EugenePetrik <eugeniy.petrik@gmail.com>
2019-09-28 15:34:50 +0300

    Add implementation for MainPage class

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,20 @@
+from .pages.main_page import MainPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
 
-    browser.get(link)
+    page = MainPage(browser, link)  # Инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                     # Открываем страницу
+    page.go_to_login_page()         # Выполняем метод страницы - переходим на страницу логина
 
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 # tb=line - выводить только одну строку из лога каждого упавшего теста
 # pytest -v --tb=line --language=en test_main_page.py
